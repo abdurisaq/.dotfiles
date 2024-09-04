6216b415076df34e0d1ff79ea01b56c43c9534e2
abdurisaq <133296208+abdurisaq@users.noreply.github.com>
2024-09-03 23:34:54 -0700

    fixed lualine config, and added more settings to clangd

--- .config/nvim/lua/duri/plugins/lsp.lua
+++ .config/nvim/lua/duri/plugins/lsp.lua
@@ -1,5 +1,5 @@
 return {
-    
+
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
@@ -11,6 +11,17 @@ return {
             ensure_installed = {"lua_ls","clangd"}
         })
         require("lspconfig").lua_ls.setup{}
-        require("lspconfig").clangd.setup{}
+        require("lspconfig").clangd.setup{
+            {
+                cmd = { "clangd", "--compile-commands-dir=./" },
+                filetypes = { "c", "cpp", "objc", "objcpp" },
+                root_dir = require("lspconfig").util.root_pattern("compile_commands.json", ".git"),
+                settings = {
+                    clangd = {
+                        -- Add extra flags for preprocessor macros
+                        compile_commands_dir = "./",
+                    },
+                },
+            }}
     end
 }
--- .zshrc
+++ .zshrc
@@ -1,1 +1,2 @@
 export PATH=$PATH:/home/duri/.local/bin
+PROMPT='%~ %# '
