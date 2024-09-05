9456d30cf47a416de8c9767122e9c368004c61d1
abdurisaq <133296208+abdurisaq@users.noreply.github.com>
2024-09-04 22:16:00 -0700

    finished setting up rudimentary lsp auto complete for c/c++

--- .config/nvim/lua/duri/plugins/lsp.lua
+++ .config/nvim/lua/duri/plugins/lsp.lua
@@ -4,15 +4,21 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+
     },
     config = function()
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {"lua_ls","clangd"}
         })
+        local cmp = require('cmp')
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
         require("lspconfig").lua_ls.setup{}
         require("lspconfig").clangd.setup{
             {
+                capabilities = lsp_capabilities,
                 cmd = { "clangd", "--compile-commands-dir=./" },
                 filetypes = { "c", "cpp", "objc", "objcpp" },
                 root_dir = require("lspconfig").util.root_pattern("compile_commands.json", ".git"),
@@ -23,5 +29,11 @@ return {
                     },
                 },
             }}
+        cmp.setup({
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'buffer' },
+            },
+        })
     end
 }
