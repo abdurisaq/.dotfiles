5473973c7b5e376677d2dceccdd4f66135df7a4e
abdurisaq <133296208+abdurisaq@users.noreply.github.com>
2024-10-11 22:01:40 -0700

    added test case to check if nvim is installed, if not pass

--- install.sh
+++ install.sh
@@ -1,33 +1,30 @@
 #!/usr/bin/env bash
-set -e  # Exit immediately if a command exits with a non-zero status
 
 NEOVIM_VERSION="v0.10.1"  # Replace with the version you want
 NEOVIM_URL="https://github.com/neovim/neovim/releases/download/${NEOVIM_VERSION}/nvim-linux64.tar.gz"
 INSTALL_DIR="/usr/local/nvim-linux64"
 
-command -v nvim >/dev/null 2>&1 && echo "Neovim is already installed at $(command -v nvim)" && exit 0
 
+if command -v nvim >/dev/null 2>&1; then
+    echo "Neovim is already installed at $(command -v nvim)"
+else
 
-# Download the tarball
-echo "Downloading Neovim ${NEOVIM_VERSION}..."
-wget $NEOVIM_URL -O nvim-linux64.tar.gz
+    echo "Downloading Neovim ${NEOVIM_VERSION}..."
+    wget $NEOVIM_URL -O nvim-linux64.tar.gz
 
-# Extract the tarball
-echo "Extracting Neovim..."
-tar -xzf nvim-linux64.tar.gz
+    echo "Extracting Neovim..."
+    tar -xzf nvim-linux64.tar.gz
 
-# Move the extracted files to /usr/local
-echo "Installing Neovim..."
-sudo mv nvim-linux64 $INSTALL_DIR
+        echo "Installing Neovim..."
+    sudo mv nvim-linux64 $INSTALL_DIR
 
-# Symlink the nvim binary to make it globally accessible
-echo "Creating symlink for Neovim..."
-sudo ln -sf $INSTALL_DIR/bin/nvim /usr/local/bin/nvim
+    echo "Creating symlink for Neovim..."
+    sudo ln -sf $INSTALL_DIR/bin/nvim /usr/local/bin/nvim
 
-# Clean up downloaded tarball
-rm -f nvim-linux64.tar.gz
+    rm -f nvim-linux64.tar.gz
 
-echo "Neovim ${NEOVIM_VERSION} installed successfully!"
+    echo "Neovim ${NEOVIM_VERSION} installed successfully!"
+fi
 
 ln -sf "$HOME/.dotfiles/.config" "$HOME/.config" && echo "Symlink Created: ~/.config -> $HOME/.dotfiles/.config" || echo "Failed to create symlink."
 
@@ -35,7 +32,10 @@ sudo apt update && sudo apt install -y zsh && echo "Zsh installed successfully."
 
 ln -sf "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc" && echo "Symlink created: ~/.zshrc -> $HOME/.dotfiles/.zshrc" || echo "Failed to create symlink."
 
+chsh -s $(which zsh) && echo "Zsh set as the default shell." || echo "Failed to set Zsh as the default shell."
 
 ln -sf "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig" && echo "Symlink created: ~/.gitconfig -> $HOME/.dotfiles/.gitconfig" || echo "Failed to create symlink."
+
+sudo apt install -y tmux && echo "Tmux installed" || echo "Tmux installation failed."
 
 ln -sf "$HOME/.dotfiles/.tmux/.tmux.conf" "$HOME/.tmux.conf" && echo "Symlink created: ~/.tmux.conf -> $HOME/.dotfiles/.tmux/.tmux.conf" || echo "Failed to create symlink."
